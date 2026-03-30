15fbb339787386c3c709e37388f92323bde45ed5
PjptRojan <prajapati.rojan101@gmail.com>
2026-03-30 12:43:11 +0545

    feat: accept message api integration

--- app/src/api/verify-code/route.ts
+++ app/src/api/verify-code/route.ts
@@ -1,8 +1,7 @@
 import dbConnect from "../../lib/dbConnect";
 import UserModel from "../../model/User";
-import {z} from "zod"
-import { usernameValidation } from "../../schemas/signUpSchema";
-import { useTransition } from "react";
+// import {z} from "zod"
+// import { usernameValidation } from "../../schemas/signUpSchema";
 
 export async function POST (request: Request) {
     await dbConnect()
